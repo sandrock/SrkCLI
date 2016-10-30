591a300631b129bd59084953ac96055ea9120d4a
sandrock <antoine.sottiau@gmail.com>
2016-10-30 10:18:20 +0100

    GenerateCryptoBytes

---
+++ SrkCLI/GenerateCryptoBytes/Program.cs
@@ -0,0 +1,106 @@
+﻿
+namespace GenerateCryptoBytes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cout = Console.Out;
+            var eout = Console.Error;
+
+            if (args.Length != 2)
+            {
+                ShowUsage(cout);
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            OutputType type;
+            if (!Enum.TryParse<OutputType>(args[0], true, out type))
+            {
+                ShowError(eout, "Invalid output type.");
+                ShowUsage(cout);
+                Environment.ExitCode = 2;
+                return;
+            }
+
+            int size;
+            if (!int.TryParse(args[1], out size) || size < 1)
+            {
+                ShowError(eout, "Invalid size.");
+                ShowUsage(cout);
+                Environment.ExitCode = 3;
+                return;
+            }
+
+            var data = new byte[size];
+            using (var provider = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                provider.GetBytes(data);
+            }
+
+            if (type == OutputType.Binary)
+            {
+                var file = Path.GetTempFileName();
+                try
+                {
+                    using (var stream = new FileStream(file, FileMode.Open, FileAccess.Write, FileShare.Read))
+                    {
+                        stream.Write(data, 0, size);
+                    }
+
+                    cout.WriteLine("Wrote binary data to:");
+                    cout.WriteLine(file);
+                }
+                catch (Exception ex)
+                {
+                    ShowError(eout, ex.Message);
+                    Environment.ExitCode = 1001;
+                }
+            }
+            else if (type == OutputType.Hexadecimal)
+            {
+                var content = BitConverter.ToString(data).Replace("-", "");
+                cout.WriteLine(content);
+            }
+            else if (type == OutputType.Base64)
+            {
+                var content = Convert.ToBase64String(data, Base64FormattingOptions.None);
+                cout.WriteLine(content);
+            }
+            else
+            {
+                ShowError(eout, "Not supported format " + type + ".");
+                Environment.ExitCode = 4;
+            }
+        }
+
+        private static void ShowError(TextWriter eout, string message)
+        {
+            eout.WriteLine(message);
+        }
+
+        private static void ShowUsage(TextWriter cout)
+        {
+            cout.WriteLine("GenerateCryptoBytes");
+            cout.WriteLine("Generates crypto random bytes using RNGCryptoServiceProvider.");
+            cout.WriteLine("Usage: GenerateCryptoBytes <format> <size>");
+            cout.WriteLine("    format:     binary, hexadecimal, base64");
+            cout.WriteLine("    size:       the number of desired bytes");
+            cout.WriteLine();
+        }
+    }
+
+    public enum OutputType
+    {
+        Binary,
+        Hexadecimal,
+        Base64,
+    }
+}
---
+++ SrkCLI/GenerateCryptoBytes/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GenerateCryptoBytes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SrkCLI")]
+[assembly: AssemblyCopyright("Copyright © SandRock 2016, provided under MIT license.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("cfc180d8-3452-4935-9ff5-2efdb3557165")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
